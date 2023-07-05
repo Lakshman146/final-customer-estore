0a0d90ebc93472799bde854dff56d61efb61d37a
Lakshman <116259479+Lakshman146@users.noreply.github.com>
2023-07-05 12:54:18 +0530

    Add files via upload

---
+++ src/eStoreProduct/DAO/walletDAO.java
@@ -0,0 +1,12 @@
+package eStoreProduct.DAO;
+
+import eStoreProduct.model.wallet;
+
+public interface walletDAO {
+	public wallet getWalletAmount(int custid);
+
+	public void updatewallet(double amt, int custid);
+
+	public boolean Createwallet(int custid);
+
+}
